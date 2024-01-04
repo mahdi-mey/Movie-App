27de5d9aac2e44de185bfd454b0417ab3784d35e
Mahdi <mahdimey17@gmail.com>
2024-01-04 13:53:25 +0330

    exported this component

--- src/WatchedSummery.js
+++ src/WatchedSummery.js
@@ -1,7 +1,7 @@
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
-  
-function WatchedSummery({ watched }) {
+
+export default function WatchedSummery({ watched }) {
     const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
     const avgUserRating = average(watched.map((movie) => movie.userRating));
     const avgRuntime = average(watched.map((movie) => movie.runtime));
